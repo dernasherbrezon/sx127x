032e098f4f31fac6dde3578ae6026e14cb09d2e0
dernasherbrezon <dernasherbrezon@gmail.com>
2023-03-18 16:54:52 +0000

    make default bit rate 4800

--- examples/receive_fsk/main/main.c
+++ examples/receive_fsk/main/main.c
@@ -104,7 +104,7 @@ void app_main() {
   ESP_ERROR_CHECK(sx127x_set_opmod(SX127x_MODE_SLEEP, SX127x_MODULATION_FSK, device));
   ESP_ERROR_CHECK(sx127x_set_frequency(437200012, device));
   ESP_ERROR_CHECK(sx127x_set_opmod(SX127x_MODE_STANDBY, SX127x_MODULATION_FSK, device));
-  ESP_ERROR_CHECK(sx127x_fsk_ook_set_bitrate(2400.0, device));
+  ESP_ERROR_CHECK(sx127x_fsk_ook_set_bitrate(4800.0, device));
   ESP_ERROR_CHECK(sx127x_fsk_ook_set_fdev(5000.0, device));
   ESP_ERROR_CHECK(sx127x_fsk_ook_set_afc_auto(SX127x_AFC_AUTO_ON, device));
   ESP_ERROR_CHECK(sx127x_fsk_ook_set_afc_bandwidth(20000.0, device));
